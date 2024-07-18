7ca40a6966d92226ddded586e2bc24536bbb9213
Arun <aruntn59@gmail.com>
2024-07-18 16:27:48 +0530

    updated drawer

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../widgets/responsive_banner.dart';
@@ -16,6 +17,11 @@ class ContactScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    double propicHegiht=screenSize.height* 0.3;
+    double propicWidth=screenSize.width* 0.40 ;
+
+
     return Scaffold(
       appBar: AppBar(
         // title: Text('Contact Me'),
@@ -25,9 +31,63 @@ class ContactScreen extends StatelessWidget {
         child: Column(
           children: [
             ResponsiveBanner(
-              mobileBanner: 'assets/banners/mobileBanner/contactmemob.jpg',
-              desktopBanner: 'assets/banners/desktopBanners/contact_me.jpg',
+              mobileBanner: 'assets/banners/mobileBanner/contactmobscreen.jpg',
+              desktopBanner: 'assets/banners/desktopBanners/contact_me_web.jpg',
             ),
+
+
+            // Stack(
+            //   children: [
+            //   Positioned(
+            //   right: screenSize.width * 0.03,
+            //   bottom: screenSize.height * 0.2,
+            //   child: GestureDetector(
+            //     onTap: () {
+            //       // Navigator.pushReplacement(
+            //       //   context,
+            //       //   MaterialPageRoute(builder: (context) => Home()),
+            //       // );
+            //     },
+            //     child: Container(
+            //       height: propicHegiht,
+            //       width: propicWidth,
+            //       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            //       constraints: BoxConstraints(minWidth:150 ),
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(50),
+            //         color: Colors.grey,
+            //         // image: DecorationImage(
+            //         //     image: AssetImage('assets/img/propic.jpg'),
+            //         //      fit: BoxFit.contain),
+            //         boxShadow: [
+            //           BoxShadow(
+            //             offset: Offset(5, 2),
+            //             color: Colors.black.withOpacity(0.70),
+            //             blurRadius: 5,
+            //           ),
+            //         ],
+            //       ),
+            //       child: Center(
+            //         child: Column(
+            //           children: [
+            //             Expanded(
+            //               child: Text(
+            //                 'Arun Narayanan',
+            //                 style: GoogleFonts.dosis(
+            //                     color: Colors.grey,
+            //                     fontSize: 20,
+            //                     fontWeight: FontWeight.bold),
+            //               ),
+            //             ),
+            //
+            //           ],
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),],)
+
+
             // SizedBox(height: 20),
             // ResponsiveWidget(
             //   mobile: MobileHomeScreen(),
--- lib/widgets/navigation_drawer.dart
+++ lib/widgets/navigation_drawer.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomNavigationDrawer extends StatelessWidget {
   @override
@@ -8,31 +12,83 @@ class CustomNavigationDrawer extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
-            child: Text('Navigation'),
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               color: Colors.black38,
+            ),
+            child: Column(
+              children: [
+                const CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage('assets/images/imagedp.jpg'), // Add a profile picture in your assets folder
+                ),
+                // Flexible(
+                //   child: Text(
+                //     'Arun Narayanan',
+                //     style: GoogleFonts.dosis(
+                //       color: Colors.grey,
+                //       fontSize: 20,
+                //       fontWeight: FontWeight.bold,
+                //     ),
+                //   ),
+                // ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        launchUrl(Uri.parse('https://www.linkedin.com/in/arun-tn-809297301/'));
+                      },
+                      icon: const Icon(FontAwesomeIcons.linkedin),
+                      color: Colors.grey,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        launchUrl(Uri.parse('https://github.com/aruntn59'));
+                      },
+                      icon: const Icon(EvaIcons.github),
+                      color: Colors.grey,
+                      splashColor: Colors.blueAccent,
+                      highlightColor: Colors.blue,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        launchUrl(Uri.parse('https://www.facebook.com/people/Arun-Sulochana-Narayanan/pfbid034Nf9wD7GEmuCg2Lvt64L2ZEE8UvK7GWcJwWzyf4DRyH9BH1dx8RrkYV9g5UJ8Ptzl/?name=xhp_nt_'));
+                      },
+                      icon: const Icon(FontAwesome.facebook_brand),
+                      color: Colors.grey,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        launchUrl(Uri.parse('https://drive.google.com/file/d/1yc7jvsmj0TDmMwcqxCvZ97gxS7se-HAs/view?usp=drive_link'));
+                      },
+                      icon: const Icon(FontAwesome.file_pdf),
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
           ListTile(
-            title: Text('Home'),
+            title: const Text('Home'),
             onTap: () {
               Navigator.pushNamed(context, '/');
             },
           ),
           ListTile(
-            title: Text('About'),
+            title: const Text('About'),
             onTap: () {
               Navigator.pushNamed(context, '/about');
             },
           ),
           ListTile(
-            title: Text('Projects'),
+            title: const Text('Projects'),
             onTap: () {
               Navigator.pushNamed(context, '/projects');
             },
           ),
           ListTile(
-            title: Text('Contact'),
+            title: const Text('Contact'),
             onTap: () {
               Navigator.pushNamed(context, '/contact');
             },
